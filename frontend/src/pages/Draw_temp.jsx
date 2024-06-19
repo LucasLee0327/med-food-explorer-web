1d620f5b8ca8e9a80cf161b876c0d4c11e68b889
LucasLee0327 <0327lucas@gmail.com>
2024-06-19 23:49:49 +0800

    Rename DRaw.jsx to Draw_temp.jsx

---
+++ frontend/src/pages/Draw_temp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Draw() {
+
+    return (
+        <>
+            <h1>Draw page for difficult-chooser</h1>
+        </>      
+    )
+}
+
+export default Draw;
