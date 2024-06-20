fa9a7c9070efc3125ad2ec0008c8799ddee64972
LucasLee0327 <0327lucas@gmail.com>
2024-06-20 23:35:09 +0800

    add addrestaurant page fix 2

--- backend/src/routes/api/v1/foods/handlers.js
+++ backend/src/routes/api/v1/foods/handlers.js
@@ -17,7 +17,8 @@ export async function getAllRestaurant(req, res) {
 }
 
 export async function createRestaurant(req, res) {
-  const { name, style, type, price, arr_time } = req.body;
+  const { newRestaurant } = req.body;
+  const { name, style, type, price, arr_time } = newRestaurant;
   console.log('Received data:', { name, style, type, price, arr_time }); // 新增這行來檢查接收到的資料
   try {
       /*
@@ -34,11 +35,11 @@ export async function createRestaurant(req, res) {
         */
       const newRestaurant = await prisma.food.create({
           data: {
-              name,
-              style,
-              type,
-              price,
-              arr_time
+              name:name,
+              style:style,
+              type:type,
+              price:price,
+              arr_time:arr_time
           }
       });
       res.status(201).json(newRestaurant);
