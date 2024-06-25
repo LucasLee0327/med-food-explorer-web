bc2b7fdc60d118b0491af605e4bf749cdee4d700
LucasLee0327 <0327lucas@gmail.com>
2024-06-26 00:36:01 +0800

    finder page modification 5

--- frontend/src/pages/Finder.jsx
+++ frontend/src/pages/Finder.jsx
@@ -1,207 +1,203 @@
-import React, { Component } from "react";
-import { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
-import services from "../services";
+import services from '../services';
 
 const containerStyle = {
-    width: '100%',
-    height: '600px'
+  width: '100%',
+  height: '600px'
 };
-const libraries = ['places'];
 
 function Finder() {
-    const [foods, setFoods] = useState([]);
-    const [filters, setFilters] = useState({
-        style: [],
-        type: [],
-        price: [],
-        arr_time: []
-    });
-    const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const [foods, setFoods] = useState([]);
+  const [filters, setFilters] = useState({
+    style: [],
+    type: [],
+    price: [],
+    arr_time: []
+  });
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
 
-    const apiKey = import.meta.env.VITE_GOOGLEMAP_API_KEY;
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: apiKey,
-        id: 'google-map-script'
-    });
+  const apiKey = import.meta.env.VITE_GOOGLEMAP_API_KEY;
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: apiKey,
+    id: 'google-map-script'
+  });
 
-    useEffect(() => {
-        // Fetch initial data
-        async function fetchData() {
-            try {
-                const food = await services.user.getAll("");
-                setFoods(food);
-            } catch (error) {
-                console.error('Error fetching restaurants:', error);
-            }
-        }
-        fetchData();
-    }, []);
-
-    const handleCheckboxChange = (event, category) => {
-        const { value, checked } = event.target;
-        if (checked) {
-            setFilters(prevFilters => ({
-                ...prevFilters,
-                [category]: [...prevFilters[category], value]
-            }));
-        } else {
-            setFilters(prevFilters => ({
-                ...prevFilters,
-                [category]: prevFilters[category].filter(item => item !== value)
-            }));
-        }
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const queryString = new URLSearchParams(
-                Object.entries(filters).reduce((acc, [key, values]) => {
-                    values.forEach(value => acc.append(key, value));
-                    return acc;
-                }, new URLSearchParams())
-            ).toString();
-
-            const food = await services.user.getAll(queryString);
-            setFoods(food);
-        } catch (error) {
-            console.error('Error fetching restaurants:', error);
-        }
-    };
-
-    const toggleFilterMenu = () => {
-        setIsFilterOpen(!isFilterOpen);
-    };
-
-    if (!isLoaded) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const food = await services.user.getAll('');
+        setFoods(food);
+      } catch (error) {
+        console.error('Error fetching restaurants:', error);
+      }
     }
+    fetchData();
+  }, []);
 
-    return (
-        <div className="flex">
-            <div className="relative w-1/4 h-screen overflow-y-scroll p-4 border-r border-gray-300 scrollbar-hide">
-                {foods.length > 0 ? (
-                    foods.map(food => (
-                        <div key={food.id} className="mb-4 p-4 bg-white shadow rounded">
-                            <h2 className="text-xl font-bold">{food.name}</h2>
-                            <p>料理形式: {food.style}</p>
-                            <p>料理類別: {food.type}</p>
-                            <p>價格: {food.price}</p>
-                            <p>抵達所需時間: {food.arr_time}</p>
-                            <p>地址: {food.address}</p>
-                        </div>
-                    ))
-                ) : (
-                    <p>No food items available.</p>
-                )}
-                <button
-                    className={`fixed top-1/2 right-0 transform -translate-y-1/2 bg-blue-500 text-white px-2 py-1 rounded transition-transform duration-300 z-50 ${isFilterOpen ? 'translate-x-64' : ''}`}
-                    onClick={toggleFilterMenu}
-                >
-                    <i className={`fas fa-chevron-right fa-2x ${isFilterOpen ? 'rotate-180' : ''}`}></i>
-                </button>
+  const handleCheckboxChange = (event, category) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setFilters(prevFilters => ({
+        ...prevFilters,
+        [category]: [...prevFilters[category], value]
+      }));
+    } else {
+      setFilters(prevFilters => ({
+        ...prevFilters,
+        [category]: prevFilters[category].filter(item => item !== value)
+      }));
+    }
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const queryString = new URLSearchParams(
+        Object.entries(filters).reduce((acc, [key, values]) => {
+          values.forEach(value => acc.append(key, value));
+          return acc;
+        }, new URLSearchParams())
+      ).toString();
+
+      const food = await services.user.getAll(queryString);
+      setFoods(food);
+    } catch (error) {
+      console.error('Error fetching restaurants:', error);
+    }
+  };
+
+  const toggleFilterMenu = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="relative w-1/4 h-full overflow-y-scroll p-4 border-r border-gray-300">
+        {foods.length > 0 ? (
+          foods.map(food => (
+            <div key={food.id} className="mb-4 p-4 bg-white shadow rounded">
+              <h2 className="text-xl font-bold">{food.name}</h2>
+              <p>料理形式: {food.style}</p>
+              <p>料理類別: {food.type}</p>
+              <p>價格: {food.price}</p>
+              <p>抵達所需時間: {food.arr_time}</p>
+              <p>地址: {food.address}</p>
             </div>
-            <div className="w-3/4 h-screen">
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={{ lat: 25.0330, lng: 121.5654 }}
-                    zoom={12}
-                >
-                    {foods.map(food => (
-                        food.latitude && food.longitude && (
-                            <Marker
-                                key={food.id}
-                                position={{
-                                    lat: food.latitude,
-                                    lng: food.longitude
-                                }}
-                                label={food.name}
-                            />
-                        )
-                    ))}
-                </GoogleMap>
-            </div>
-            <div
-                className={`fixed top-0 right-0 bg-white w-1/4 h-full shadow-lg transition-transform duration-300 z-40 ${isFilterOpen ? 'translate-x-0' : 'translate-x-full'}`}
-            >
-                <div className="p-4 overflow-y-auto h-full">
-                <form onSubmit={handleSubmit}>
-                    <fieldset className="mb-4">
-                    <legend className="text-lg font-bold mb-2">料理形式:</legend>
-                    {['中式', '西式', '日式', '韓式', '台式', '其他'].map(style => (
-                        <label key={style} className="block mb-2">
-                        <input
-                            type="checkbox"
-                            name="style"
-                            value={style}
-                            onChange={e => handleCheckboxChange(e, 'style')}
-                            checked={filters.style.includes(style)}
-                            className="mr-2"
-                        />
-                        {style}
-                        </label>
-                    ))}
-                    </fieldset>
+          ))
+        ) : (
+          <p>No food items available.</p>
+        )}
+      </div>
+      <button
+        className={`fixed top-1/2 right-0 transform -translate-y-1/2 bg-blue-500 text-white px-2 py-1 rounded transition-transform duration-300 z-50 ${isFilterOpen ? 'translate-x-64' : ''}`}
+        onClick={toggleFilterMenu}
+      >
+        <i className={`fas fa-chevron-right fa-2x ${isFilterOpen ? 'rotate-180' : ''}`}></i>
+      </button>
+      <div className="w-3/4 h-full">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={{ lat: 25.033, lng: 121.5654 }}
+          zoom={12}
+        >
+          {foods.map(food => (
+            food.latitude && food.longitude && (
+              <Marker
+                key={food.id}
+                position={{
+                  lat: food.latitude,
+                  lng: food.longitude
+                }}
+                label={food.name}
+              />
+            )
+          ))}
+        </GoogleMap>
+      </div>
+      <div
+        className={`fixed top-0 right-0 bg-white w-1/4 h-full shadow-lg transition-transform duration-300 z-40 ${isFilterOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        <div className="p-4 overflow-y-auto h-full">
+          <form onSubmit={handleSubmit}>
+            <fieldset className="mb-4">
+              <legend className="text-lg font-bold mb-2">料理形式:</legend>
+              {['中式', '西式', '日式', '韓式', '台式', '其他'].map(style => (
+                <label key={style} className="block mb-2">
+                  <input
+                    type="checkbox"
+                    name="style"
+                    value={style}
+                    onChange={e => handleCheckboxChange(e, 'style')}
+                    checked={filters.style.includes(style)}
+                    className="mr-2"
+                  />
+                  {style}
+                </label>
+              ))}
+            </fieldset>
 
-                    <fieldset className="mb-4">
-                    <legend className="text-lg font-bold mb-2">料理類別:</legend>
-                    {['飯', '麵', '麵包', '其他'].map(type => (
-                        <label key={type} className="block mb-2">
-                        <input
-                            type="checkbox"
-                            name="type"
-                            value={type}
-                            onChange={e => handleCheckboxChange(e, 'type')}
-                            checked={filters.type.includes(type)}
-                            className="mr-2"
-                        />
-                        {type}
-                        </label>
-                    ))}
-                    </fieldset>
+            <fieldset className="mb-4">
+              <legend className="text-lg font-bold mb-2">料理類別:</legend>
+              {['飯', '麵', '麵包', '其他'].map(type => (
+                <label key={type} className="block mb-2">
+                  <input
+                    type="checkbox"
+                    name="type"
+                    value={type}
+                    onChange={e => handleCheckboxChange(e, 'type')}
+                    checked={filters.type.includes(type)}
+                    className="mr-2"
+                  />
+                  {type}
+                </label>
+              ))}
+            </fieldset>
 
-                    <fieldset className="mb-4">
-                    <legend className="text-lg font-bold mb-2">價格:</legend>
-                    {['便宜', '中等', '貴'].map(price => (
-                        <label key={price} className="block mb-2">
-                        <input
-                            type="checkbox"
-                            name="price"
-                            value={price}
-                            onChange={e => handleCheckboxChange(e, 'price')}
-                            checked={filters.price.includes(price)}
-                            className="mr-2"
-                        />
-                        {price}
-                        </label>
-                    ))}
-                    </fieldset>
+            <fieldset className="mb-4">
+              <legend className="text-lg font-bold mb-2">價格:</legend>
+              {['便宜', '中等', '貴'].map(price => (
+                <label key={price} className="block mb-2">
+                  <input
+                    type="checkbox"
+                    name="price"
+                    value={price}
+                    onChange={e => handleCheckboxChange(e, 'price')}
+                    checked={filters.price.includes(price)}
+                    className="mr-2"
+                  />
+                  {price}
+                </label>
+              ))}
+            </fieldset>
 
-                    <fieldset className="mb-4">
-                    <legend className="text-lg font-bold mb-2">抵達所需時間:</legend>
-                    {['馬上', '5分', '10分', '20分', '30分以上'].map(arr_time => (
-                        <label key={arr_time} className="block mb-2">
-                        <input
-                            type="checkbox"
-                            name="arr_time"
-                            value={arr_time}
-                            onChange={e => handleCheckboxChange(e, 'arr_time')}
-                            checked={filters.arr_time.includes(arr_time)}
-                            className="mr-2"
-                        />
-                        {arr_time}
-                        </label>
-                    ))}
-                    </fieldset>
+            <fieldset className="mb-4">
+              <legend className="text-lg font-bold mb-2">抵達所需時間:</legend>
+              {['馬上', '5分', '10分', '20分', '30分以上'].map(arr_time => (
+                <label key={arr_time} className="block mb-2">
+                  <input
+                    type="checkbox"
+                    name="arr_time"
+                    value={arr_time}
+                    onChange={e => handleCheckboxChange(e, 'arr_time')}
+                    checked={filters.arr_time.includes(arr_time)}
+                    className="mr-2"
+                  />
+                  {arr_time}
+                </label>
+              ))}
+            </fieldset>
 
-                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">篩選</button>
-                </form>
-                </div>
-            </div>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">篩選</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Finder;
