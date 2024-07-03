e32a301d00357862d4bb380c8f517d5392f35bfb
LucasLee0327 <0327lucas@gmail.com>
2024-07-03 20:19:14 +0800

    phone version 6

--- frontend/src/pages/Draw.jsx
+++ frontend/src/pages/Draw.jsx
@@ -204,10 +204,10 @@ function Draw() {
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
                     <div className="bg-white p-4 rounded shadow-lg w-11/12 h-3/4 relative flex flex-col md:flex-row">
                         <button
-                            className="absolute bottom-2 left-2 bg-gray-600 text-white font-bold px-4 py-2 rounded"
+                            className="absolute top-1 left-1 bg-gray-600 text-white font-bold px-1 py-1 rounded"
                             onClick={closePopup}
                         >
-                            確定
+                            &time;
                         </button>
                         <div className="p-4 overflow-y-auto md:w-1/2">
                             <h2 className="text-xl font-bold mb-4">{selectedRestaurant.name}</h2>
